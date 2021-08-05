c14ac9ffc62e77af3c40d8f7a030c81fd357c10b
Ruth Kurniawati <rkurniawati@westfield.ma.edu>
2021-08-05 18:08:55 -0400

    OpenMP examples using Pyjama in Java: 00 and 01

---
+++ Patternlets/pyjama/01.forkJoin2/ForkJoin2.java
@@ -0,0 +1,41 @@
+/* ForkJoin2.java
+ * ... illustrates the fork-join pattern 
+ *      using multiple OpenMP parallel directives,
+ *      and changing the number of threads two ways.
+ *
+ * Joel Adams, Calvin College, May 2013.
+ * Adapted for Java by Ruth Kurniawati, Westfield State University, July, 2021
+ *
+ * Usage: 
+ *   make run
+ *
+ * Exercise:
+ * - Compile & run, compare results to source.
+ * - Predict how many threads will be used in 'Part IV'?
+ * - Uncomment 'Part IV', recompile, rerun.
+ */
+
+class ForkJoin2 {
+    public static void main(String[] args) {
+
+        System.out.print("\nBeginning\n");
+
+        //#omp parallel 
+        System.out.print("\nPart I");
+
+        System.out.print("\n\nBetween I and II...\n");
+
+        Pyjama.omp_set_num_threads(3);
+
+        //#omp parallel 
+        System.out.print("\nPart II...");
+
+        System.out.print("\n\nBetween II and III...\n");
+
+        //#omp parallel num_threads(5)
+        System.out.print("\nPart III...");
+
+        System.out.print("\n\nEnd\n\n");
+    }
+}
+

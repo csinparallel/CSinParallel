107c36ee5e48666b96798d44719148ed8b2e4bfb
Ruth Kurniawati <rkurniawati@westfield.ma.edu>
2021-08-07 12:59:51 -0400

    Added Barrier and MasterWorker. Added a Makefile to pull Pyjama lib.

---
+++ Patternlets/pyjama/04.barrier/Barrier.java
@@ -0,0 +1,35 @@
+/* barrier.c
+ * ... illustrates the use of the OpenMP barrier command,
+ * 	using the commandline to control the number of threads...
+ *
+ * Joel Adams, Calvin College, May 2013.
+ *
+ * Usage: ./barrier [numThreads]
+ * 
+ * Exercise:
+ * - Compile & run several times, noting interleaving of outputs.
+ * - Remove the barrier directive, recompile, rerun,
+ *    and note the change in the outputs.
+ */
+class Barrier {
+    public static void  main(String[] args) {
+        if (args.length >= 1) {
+            Pyjama.omp_set_num_threads( Integer.parseInt(args[0]) );
+        }
+
+        System.out.println();
+        //#omp parallel 
+        {
+            int id = Pyjama.omp_get_thread_num();
+            int numThreads = Pyjama.omp_get_num_threads();
+            System.out.println("Thread "+id + " of "+numThreads+" is BEFORE the barrier.");
+
+            //Try this with and without the barrier
+            //#omp barrier 
+
+            System.out.println("Thread "+id + " of "+numThreads+" is AFTER the barrier.");
+        }
+
+        System.out.println();
+    }
+}
---
+++ Patternlets/pyjama/05.masterWorker/MasterWorker.java
@@ -0,0 +1,37 @@
+/* masterWorker.c
+ * ... illustrates the master-worker pattern in OpenMP
+ *
+ * Joel Adams, Calvin College, November 2009.
+ *
+ * Usage: make run
+ *
+ * Exercise: 
+ * - Compile and run as is.
+ * - Uncomment #pragma directive, re-compile and re-run
+ * - Compare and trace the different executions.
+ */
+
+class MasterWorker {
+    public static void main(String[] args) {
+        if (args.length >= 1) {
+            Pyjama.omp_set_num_threads(Integer.parseInt(args[0]));
+        }
+        System.out.println();
+
+
+        //#omp parallel 
+        {
+            int id = Pyjama.omp_get_thread_num();
+            int numThreads = Pyjama.omp_get_num_threads();
+
+            if ( id == 0 ) {  // thread with ID 0 is master
+                System.out.println("Greetings from the master, #"+ id +" of " +  numThreads + " threads");
+            } else {          // threads with IDs > 0 are workers 
+                System.out.println("Greetings from a worker, #"+ id +" of " +  numThreads + " threads");
+            }
+        }
+
+        System.out.println();
+    }
+}
+

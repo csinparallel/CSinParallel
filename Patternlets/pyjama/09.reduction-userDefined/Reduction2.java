3b019e3215d50e91c0739ddb4d95c94614b06db1
Ruth Kurniawati <rkurniawati@westfield.ma.edu>
2021-08-10 21:34:39 -0400

    Using long in the factorial calc and multiplication operator in the reduction. Not very interesting since we will get overflow when n > 20

--- Patternlets/pyjama/09.reduction-userDefined/Reduction2.java
+++ Patternlets/pyjama/09.reduction-userDefined/Reduction2.java
@@ -1,10 +1,15 @@
 /* Reduction2.java computes a table of factorial values,
- *  OpenMP's user-defined reductions.
+ *  OpenMP's reduction using the multiplication operator.
  *
  *  Joel Adams, Calvin College, December 2015.
+ *  Adapted for Java/Pyjama by Ruth Kurniawati, Westfield State University, July, 2021
  *
- *  Usage: ./reduction2 [numThreads] [n]
+ *  Usage: 
+ *    make run
+ *    make run ARGS="numThreads n"
+ *    For example: make run ARGS="4 100" 
  *
+ * 
  *  Exercise:
  *  - Build and run, record sequential time in a spreadsheet
  *  - Uncomment #pragma omp parallel for directive, rebuild,
@@ -17,50 +22,19 @@
  *  - Experiment with different n values
  */
 
-import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.IntStream;
 
 public class Reduction2 {
-    static class MyBigInteger {
-        private BigInteger myValue;
-
-        public MyBigInteger() {
-            myValue = BigInteger.valueOf(1);
-        }
-
-        public MyBigInteger(int val) {
-            myValue = BigInteger.valueOf(val);
-        }
-
-        public MyBigInteger(BigInteger val) {
-            myValue = val;
-        }
-
-        public MyBigInteger multiply(MyBigInteger arg) {
-            return new MyBigInteger(myValue.multiply(arg.myValue));
-        }
-
-        @Override
-        public String toString() {
-            return myValue.toString();
-        }
-    }
-
-    static MyBigInteger multiply(MyBigInteger arg1, MyBigInteger arg2) {
-        return arg1.multiply(arg2);
-    }
-
-    static MyBigInteger factorial(int numThreads, int n) {
-        MyBigInteger acc = new MyBigInteger(1);
+    static long factorial(int numThreads, int n) {
+        long acc = 1;
 
         //#omp parallel shared(acc, n) num_threads(numThreads) 
         {
-            //#omp for reduction(multiply:acc)
+            //#omp for reduction(*:acc)
             for(int i=2; i <= n; i++) {
-                MyBigInteger x = new MyBigInteger(i);
-                acc = acc.multiply(x);
+                acc *= i;
             }
         }
         return acc;
@@ -76,16 +50,20 @@ public class Reduction2 {
             numThreads = Integer.parseInt(args[0]);
         }
 
-        int n = 32;
+        int n = 20;
         if (args.length == 2) {
             n = Integer.parseInt(args[1]);
+            if (n > 20) {
+                System.out.println("n cannot be greater than 20 (overflow will happen)");
+                return;
+            }
         }
 
         long startTime = System.currentTimeMillis();
-        MyBigInteger result = factorial(numThreads, n);
+        long result = factorial(numThreads, n);
         long duration = System.currentTimeMillis() - startTime;
 
-        System.out.println("Result = " + result.toString());
+        System.out.println("Result = " + result);
         System.out.println("Time = " + duration + " ms");
     }
 }

7061576c07bef24aba113c66c5386a2771e2e360
Ruth Kurniawati <rkurniawati@westfield.ma.edu>
2021-12-05 23:58:40 -0500

    Removed the additional longer ligands

--- Exemplars/DrugDesign/DDPyjama.java
+++ Exemplars/DrugDesign/DDPyjama.java
@@ -6,7 +6,7 @@ public class DDPyjama {
     static String[] cannedLigands = 
         {"razvex", "qudgy", "afrs", "sst", "pgfht", "rt", 
         "id", "how", "aaddh",  "df", "os", "hid", 
-        "sad", "fl", "rd", "edp", "dfgt", "spa", "qdsayjbp", "zunerd"};
+        "sad", "fl", "rd", "edp", "dfgt", "spa"};
 
     // Ligand Score pair
     static class LSPair {
